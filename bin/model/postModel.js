a363c1675d12dde58fbf9eef288a6f563fbc11cb
juninmd <jr_acn@yahoo.com.br>
2017-09-26 21:28:58 -0300

    retirada de categoria

--- bin/model/postModel.js
+++ bin/model/postModel.js
@@ -5,6 +5,5 @@ module.exports = db.model('post', new Schema(
 		autor: { type: String, default: '', trim: true },
 		horario: { type: Date, default: '', trim: true },
 		descricao: { type: String, default: '', trim: true },
-		status: { type: Number, default: '', trim: true },
-		categoria: { type: Number, default: '', trim: true }
+		status: { type: Number, default: '', trim: true }
 	}))
